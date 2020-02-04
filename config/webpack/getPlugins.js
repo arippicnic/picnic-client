82ce2f6565019cc4e0597183619d77a3852163e5
aripmusic <arifdwimusic@gmail.com>
2020-02-04 11:12:45 +0700

    clean-webpack-plugin

--- config/webpack/getPlugins.js
+++ config/webpack/getPlugins.js
@@ -2,11 +2,15 @@ import path from "path";
 import ManifestPlugin from "webpack-manifest-plugin";
 import LoadablePlugin from "@loadable/webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
 
 export default ({ isDev, webpack }) => {
 	const plugins = [
 		new ManifestPlugin({
-			fileName: path.resolve(process.cwd(), "build/public/webpack-assets.json"),
+			fileName: path.resolve(
+				process.cwd(),
+				"build/public/webpack-assets.json"
+			),
 			filter: file => file.isInitial
 		}),
 		new LoadablePlugin({
@@ -20,7 +24,8 @@ export default ({ isDev, webpack }) => {
 		new webpack.DefinePlugin({
 			"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
 			"process.env.GRAPH_URL": JSON.stringify(process.env.GRAPH_URL)
-		})
+		}),
+		new CleanWebpackPlugin()
 	];
 	if (isDev) {
 		plugins.push(new webpack.HotModuleReplacementPlugin());
