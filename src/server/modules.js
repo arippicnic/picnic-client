43d01fe9a86445009f1ee71b76782ffd0913dd9a
aripmusic <arifdwimusic@gmail.com>
2020-02-14 23:05:19 +0700

    add body parser

--- src/server/modules.js
+++ src/server/modules.js
@@ -3,13 +3,15 @@ import helmet from 'helmet';
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const { NODE_ENV } = process.env;
 
 export default app => {
 	app.use(helmet());
 	app.use(cookieParser());
-
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(cors());
 	app.use(express.static(path.resolve(process.cwd(), "build/public")));
 	if (NODE_ENV === "development") {
