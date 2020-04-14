559ae2314655c82fc0007bc30a37a0abacb34181
aripmusic <arifdwimusic@gmail.com>
2020-04-15 01:56:38 +0700

    optimize 2

--- src/client.js
+++ src/client.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from "redux";
 import { ApolloProvider } from "react-apollo";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
+import { AppContainer } from "react-hot-loader";
 import { renderRoutes } from "react-router-config";
 import { loadableReady } from "@loadable/component";
 import { BrowserRouter } from "react-router-dom";
@@ -12,6 +13,9 @@ import { BrowserRouter } from "react-router-dom";
 import client from "./services/apollo";
 import routes from "./routes";
 import reducers from "./reducers";
+
+// import './assets/css/bootstrap.min.css';
+import './assets/sass/main.scss';
 
 const store = createStore(
   reducers,
@@ -22,10 +26,13 @@ const store = createStore(
 );
 
 const render = Routes => {
-  ReactDOM.hydrate(
+  const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate;
+  renderMethod(
     <Provider store={store}>
       <ApolloProvider client={client}>
-        <BrowserRouter>{renderRoutes(Routes)}</BrowserRouter>
+        <AppContainer>
+          <BrowserRouter>{renderRoutes(Routes)}</BrowserRouter>
+        </AppContainer>
       </ApolloProvider>
     </Provider>,
     document.getElementById("root")
@@ -35,3 +42,15 @@ const render = Routes => {
 loadableReady(() => {
   render(routes);
 });
+
+if (module.hot) {
+  module.hot.accept("./routes", () => {
+    try {
+      const nextRoutes = require("./routes").default;
+
+      render(nextRoutes);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
